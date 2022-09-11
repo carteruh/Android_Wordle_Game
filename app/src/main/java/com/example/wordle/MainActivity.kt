3aff6f682d238bb466920ae3856438fe779c7314
Carter Ung <carterung@gmail.com>
2022-09-11 10:37:08 -0500

    Hides Keyboard

--- app/src/main/java/com/example/wordle/MainActivity.kt
+++ app/src/main/java/com/example/wordle/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.example.wordle
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.view.inputmethod.InputMethodManager
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
@@ -24,10 +26,9 @@ class MainActivity : AppCompatActivity() {
         var ansString = FourLetterWordList.getRandomFourLetterWord().toString().uppercase()
         answer.text = ansString
         input.setOnClickListener{
-
             button.setOnClickListener {
+                closeKeyBoard(input)
                 var stringText = input.text.toString().uppercase()
-
                 if (count == 1){
                     ans1.text = stringText
                     ans1.visibility = View.VISIBLE
@@ -58,6 +59,11 @@ class MainActivity : AppCompatActivity() {
 
     }
 
+
+    private fun closeKeyBoard(view: View){
+        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(view.windowToken,0)
+    }
     /**
      * Parameters / Fields:
      *   wordToGuess : String - the target word the user is trying to guess
